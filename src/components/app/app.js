962b5fe75dac1f3f5dd1d0e8345ee3e9fd13dbb8
JustgoodDeal <stostoraz@gmail.com>
2021-01-21 17:03:31 +0400

    removed unused code

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -128,4 +128,3 @@ export default class App extends Component {
         );
     }
 }
-
--- src/components/todo-list-item/todo-list-item.js
+++ src/components/todo-list-item/todo-list-item.js
@@ -33,64 +33,6 @@ const TodoListItem = ({ label, delFunc, importantFunc, doneFunc, important, done
           </button>
         </span>
         )
-}
+};
 
 export default TodoListItem;
-
-// export default class TodoListItem extends Component {
-//
-//     state = {
-//         done: false,
-//         important: false
-//     };
-//
-//     onLabelClick = () => {
-//         this.setState( ({done}) => {
-//             return {
-//                 done: !done
-//             }
-//         });
-//     };
-//
-//     onMarkImportant = () => {
-//         this.setState( ({important}) => {
-//             return {
-//                 important: !important
-//             }
-//         });
-//     };
-//
-//     render() {
-//         let { label, delFunc } = this.props;
-//         let { done, important  } = this.state;
-//         let classNames = 'todo-list-item';
-//
-//         if (done) {
-//             classNames += ' done'
-//         }
-//
-//         if (important) {
-//             classNames += ' important'
-//         }
-//
-//         return (
-//             <span className={classNames}>
-//               <span
-//                   className="todo-list-item-label"
-//                   onClick={ this.onLabelClick } >
-//                 {label}
-//               </span>
-//               <button type="button"
-//                       className="btn btn-outline-success btn-sm float-right"
-//                       onClick={ this.onMarkImportant } >
-//                 <i className="fa fa-exclamation"/>
-//               </button>
-//               <button type="button"
-//                       className="btn btn-outline-danger btn-sm float-right"
-//                       onClick={ delFunc } >
-//                 <i className="fa fa-trash-o"/>
-//               </button>
-//             </span>
-//         )
-//     }
-// }
